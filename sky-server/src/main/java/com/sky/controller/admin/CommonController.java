a72d7d2f0b97eb8202fec5e01909ac0863ac0342
flowerchar <flowerchar2001@gmail.com>
2023-08-21 17:54:03 +0800

    feat: 新增菜品-注册AliOss，填入相关信息

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,20 +1,40 @@
 package com.sky.controller.admin;
 
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/admin/common")
 @Api(tags = "通用接口")
 public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
     @PostMapping("/upload")
     @ApiOperation("文件上传")
     public Result<String> upload(MultipartFile file){
+
+
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + extension;
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
