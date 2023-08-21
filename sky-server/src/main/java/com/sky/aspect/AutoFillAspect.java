9192a1b85fcb4b19e72e6e9d34dbe4f68f720379
flowerchar <flowerchar2001@gmail.com>
2023-08-21 15:45:54 +0800

    feat: 公共字段自动填充-代码开发1

---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,22 @@
+package com.sky.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class AutoFillAspect {
+
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
+    public void autoFillPointCut(){}
+
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint){
+        
+    }
+}
