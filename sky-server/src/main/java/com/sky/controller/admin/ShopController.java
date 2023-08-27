d34eebcfb772c621b89d550527315f2ad9a2b6f8
flowerchar <flowerchar2001@gmail.com>
2023-08-27 22:24:37 +0800

    feat: test git

--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -7,25 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+// 增加了一个没用的代码来测试git 
 @RestController("adminShopController")
 @RequestMapping("/admin/shop")
 @Api(tags = "店铺相关接口")
 public class ShopController {
-    public static final String KEY="SHOP_STATUS";
+    public static final String KEY = "SHOP_STATUS";
 
     @Autowired
     private RedisTemplate redisTemplate;
 
     @PutMapping("/{status}")
     @ApiOperation("设置店铺营业状态")
-    public Result setStatus(@PathVariable Integer status){
+    public Result setStatus(@PathVariable Integer status) {
         redisTemplate.opsForValue().set(KEY, status);
         return Result.success();
     }
 
     @GetMapping("/status")
     @ApiOperation("获取店铺营业状态")
-    public Result<Integer> getStatus(){
+    public Result<Integer> getStatus() {
         Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
         return Result.success(status);
     }
