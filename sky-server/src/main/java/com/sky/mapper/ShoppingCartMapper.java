8d0c24eebd1f2aaaa99650eeea0a19fee4f69a33
flowerchar <flowerchar2001@gmail.com>
2023-08-28 16:04:10 +0800

    fix: 添加购物车-代码开发3-修复mapper的insert代码

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -15,7 +15,7 @@ public interface ShoppingCartMapper {
     @Update("update shopping_cart set number = #{number} where id = #{id}")
     void updateNumberById(ShoppingCart shoppingCart);
 
-    @Insert("insert into shopping_cart(name, user_id, setmeal_id, dish_flavor, number, amount, imgae, create_time)" +
-            "values (#{name}, #{userId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{imgae}, #{createTime}) ")
+    @Insert("insert into shopping_cart(name, user_id,dish_id, setmeal_id, dish_flavor, number, amount, image, create_time)" +
+            "values (#{name}, #{userId},#{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{image}, #{createTime}) ")
     void insert(ShoppingCart shoppingCart);
 }
