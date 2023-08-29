5a401970763be666a45fa99adad6b72a99d66635
flowerchar <flowerchar2001@gmail.com>
2023-08-29 21:58:21 +0800

    feat: Day9

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -22,4 +22,11 @@ public interface ShoppingCartMapper {
 
     @Delete("delete from shopping_cart where user_id=#{userId}")
     void deleteByUserId(Long userId);
+
+    /**
+     * 批量插入购物车数据
+     *
+     * @param shoppingCartList
+     */
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
