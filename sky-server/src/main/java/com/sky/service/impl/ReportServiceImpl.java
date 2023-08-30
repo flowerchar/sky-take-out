edfc9061a44f1525c3b86a5b2c1fe95421bbf23a
flowerchar <flowerchar2001@gmail.com>
2023-08-30 21:07:26 +0800

    feat: 营业额统计-代码开发2

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -3,19 +3,30 @@ package com.sky.service.impl;
 import com.sky.mapper.OrderMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
-    
+
     @Override
     public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
-        return null;
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while (!begin.equals(end)) {
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        return TurnoverReportVO.builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .build();
     }
 }
