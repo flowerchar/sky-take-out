9289b6dedfc6a42f50e6242536d3f6e7ee978c50
flowerchar <flowerchar2001@gmail.com>
2023-08-30 20:10:14 +0800

    feat: 客户催单

--- sky-server/src/main/java/com/sky/controller/user/UserController.java
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -5,16 +5,14 @@ import com.sky.dto.UserLoginDTO;
 import com.sky.entity.User;
 import com.sky.properties.JwtProperties;
 import com.sky.result.Result;
+import com.sky.service.OrderService;
 import com.sky.service.UserService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.UserLoginVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -27,6 +25,8 @@ public class UserController {
     private UserService userService;
     @Autowired
     private JwtProperties jwtProperties;
+    @Autowired
+    private OrderService orderService;
 
     @PostMapping("/login")
     @ApiOperation("微信登录")
@@ -42,5 +42,12 @@ public class UserController {
                 .build();
 
         return Result.success(userLoginVO);
+    }
+
+    @ApiOperation("客户催单")
+    @GetMapping("/reminder/{id}")
+    public Result reminder(@PathVariable("id") Long id) {
+        orderService.reminder(id);
+        return Result.success();
     }
 }
